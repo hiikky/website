2398e32526c149847c5d49e877d348df91e73e6e
GROVE4L <grove4l_2013@hotmail.com>
2021-08-08 18:12:14 -0300

    Aporte de Guidosaby - Cambio de background en clase results - Acomodadas lineas del main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 let ubicacionPrincipal = window.pageYOffset;
 
-
     AOS.init();
-
 
 window.addEventListener("scroll", function(){
     let desplazamientoActual = window.pageYOffset;
-    if(ubicacionPrincipal>=desplazamientoActual){
-        document.getElementsByTagName("nav") [0].style.top = "0px";
-    }else{
-        document.getElementsByTagName("nav") [0].style.top = "-100px";
+    if(ubicacionPrincipal >= desplazamientoActual) {
+        document.getElementsByTagName("nav")[0].style.top = "0px";
     }
-    ubicacionPrincipal= desplazamientoActual;
+    else {
+        document.getElementsByTagName("nav")[0].style.top = "-100px";
+    }
+    ubicacionPrincipal = desplazamientoActual;
 })
 
 //Menu
@@ -20,10 +19,11 @@ let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
 let semaforo = true;
 
 document.querySelectorAll(".hamburger")[0].addEventListener("click", function(){
-    if(semaforo){
+    if(semaforo) {
         document.querySelectorAll(".hamburger")[0].style.color="#fff";
         semaforo=false;
-    }else{
+    }
+    else {
         document.querySelectorAll(".hamburger")[0].style.color="#000";
         semaforo=true;
     }
